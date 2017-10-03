701720dd41493a65f22c2480141ac068f037abc9
Zack Harvey <zackh99@gmail.com>
2017-10-03 18:35:14 -0300

    Setup Production Webpack Build

--- src/app.js
+++ src/app.js
@@ -14,8 +14,6 @@ import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 
-import getVisibleExpenses from './selectors/expenses';
-
 const store = configureStore();
 
 const jsx = (
